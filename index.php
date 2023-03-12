5090d312da781e48994096d8741646b020cb7e68
Sasza <alewesij@onet.pl>
2023-03-12 11:47:20 +0100

    Add html

--- index.php
+++ index.php
@@ -12,4 +12,48 @@ namespace App;
 
 include_once('./src/utils/debug.php');
 
-dump('test');
+if(!empty($_GET['action'])){
+    $action = $_GET['action'];
+} else {
+    $action = null;
+}
+
+dump($_GET);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My notes</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <header>
+        <h1>Moje notatki</h1>    
+    </header>
+
+    <main>
+        <nav>
+            <ul class="ul-note">
+                <li class="li-note">
+                    <a href="/">Nowa notatka</a>
+                </li>
+                <li class="li-note">
+                    <a href="/?action=create">Lista notatek</a>
+                </li>
+            </ul>
+        </nav>
+        <article>
+            <?php if ($action === 'create') : ?>
+                <h3>Nowa notatka</h3>
+            <?php else : ?>
+                <h3>Lista notatek</h3>
+            <?php endif; ?>
+        </article>
+    </main>
+</body>
+</html>
