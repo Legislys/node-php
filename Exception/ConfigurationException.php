12b76f5ea6b58d64cb79a880bd6df80a5e09096b
Sasza Węsierski <alewesij@onet.pl>
2023-04-15 11:16:06 +0200

    Add exceptions

---
+++ Exception/ConfigurationException.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+namespace App\Exception;
+
+require_once('AppException.php');
+
+use Exception;
+use App\Exception\AppException;
+
+class ConfigurationException extends AppException{}
