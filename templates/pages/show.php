382107bcf7fcaf3247f1f2b510b836e798bc0297
Sasza Węsierski <alewesij@onet.pl>
2023-04-23 09:46:07 +0200

    Add edit page

--- templates/pages/show.php
+++ templates/pages/show.php
@@ -1,14 +1,19 @@
 <div class="show">
     <?php $note = $params['note'] ?? null; ?>
-    <?php if($note) : ?>
+    <?php if ($note) : ?>
         <ul>
             <li> Id: <?php echo (int) $note['id'] ?></li>
             <li> Tytuł: <?php echo htmlentities($note['title']); ?></li>
             <li> Opis: <?php echo htmlentities($note['description']); ?></li>
-            <li> Utworzono: <?php echo (int) htmlentities( $note['created']); ?></li>
+            <li> Utworzono: <?php echo (int) htmlentities($note['created']); ?></li>
             <li>
                 <a href="/">
                     <button>Powrót do listy notatek</button>
+                </a>
+            </li>
+            <li>
+                <a href="/?action=edit&id=<?php echo $note['id']; ?>">
+                    <button>Edytuj</button>
                 </a>
             </li>
         </ul>
